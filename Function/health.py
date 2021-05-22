f6c44339a93bd02b24ad458ef79103eb84d87f4f
RenDjunyu <2205997481@qq.com>
2021-05-22 10:04:49 +0800

    full

--- Function/health.py
+++ Function/health.py
@@ -1,8 +1,48 @@
 import requests
 import re
 
-user="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36 Edg/86.0.622.43"
-referer = 'https://healthreport.zju.edu.cn/'
+'''
+:authority: healthreport.zju.edu.cn
+:method: POST
+:path: /ncov/wap/default/save
+:scheme: https
+accept: application/json, text/javascript, */*; q=0.01
+accept-encoding: gzip, deflate, br
+accept-language: zh-CN,zh;q=0.9
+content-length: 2289
+content-type: application/x-www-form-urlencoded; charset=UTF-8
+cookie: eai-sess=6q4k27b1sp3d165tf6sofq6hj1; UUkey=a79e4fa906f16ab0a5fb0f5823d15f51; _csrf=S8mwplVi9KWoF2WQ0TlCeCDx52iPwPC81Z3pGJBnEPQ%3D; _pv0=5qvg4K3Pqa4vj6AG9reS8kMBvB5WKvdcwRTZxK9cYIZ%2FZBWg16hwyRynzzOUbECeMsEQCI386Tvn5aoOlt4gIFELcvBzNXztwOPvXBb9soVKPDGERGyFPAGSgnPCFJMWrG0leYb9tjcmJTOBJkRCfuPJv6nTYzS0Y5Xc7MGoFLMQnO3nMBly6QWbV45uGp5r3%2BkXYFTCWMTPIjAIp3U88DGNmpCUx5r5naHQAMDa8Lo3L6zfUjcOOAvWS6YmNCF4rm3dfHiT24XGjQb1ITcz%2FjWhkO0j%2BfgX3XELHaEhsEH2syfuPDDtc0aVAEgx76D38jNKkrTo5eT5cdoPXfHcilDYeGHcD8%2BLziilkL6fkINf5YmxbVaCekyUyWDKM8WsdI1IveEf2T%2FHAvZPfGhSJZb3UYPaAx3kt3Acurem4dU%3D; _pf0=Euqigi%2F%2Bsd5pE8lKGpe%2BuN7yemwlx0dfD%2Fi9isu%2BTpc%3D; _pc0=cjkYca4h2r1qV4GDJr2gvxSHMXlITtq0aTkgfB%2BE2WYbK51FAdbzWsx%2BQzpJpDqw; iPlanetDirectoryPro=aFqObjowpnwoK0GeV7ksckc3LrX7goco4RPUcaKZQtPPXeAYbrrSgYwBVqKBviuWzVOIBe94xO1TRAu6X2NeJuPMepbgxc1g7efHJK2LtKYWCZZOPH4zVz7Cu3vChb%2BhMNxQNCi1eOTC9WoAPZdRWMXmodE9%2FGLI5yzu7Wg%2BkYdV7LfpDf4FCfV1cgWtyK4s0Aa9mvmc1o2H1jpHu8gkUCb3nM1WSG9H1nxGR0bbRVEM3PT4nXyEVWITkNhSAOp%2F%2BH6L72kye9%2FSddLabmrgFJjV21pA%2FNTN581xXfH9vkWsTWXStV6rjV%2BZjmnyiYc1lHRUqRtjrERSgtQ2aI%2BoIzyK4UBrDrflZbgeL9gmSAw%3D; Hm_lvt_48b682d4885d22a90111e46b972e3268=1621642368; Hm_lpvt_48b682d4885d22a90111e46b972e3268=1621642370
+origin: https://healthreport.zju.edu.cn
+referer: https://healthreport.zju.edu.cn/ncov/wap/default/index?from=history
+sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"
+sec-ch-ua-mobile: ?0
+sec-fetch-dest: empty
+sec-fetch-mode: cors
+sec-fetch-site: same-origin
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36
+x-requested-with: XMLHttpRequest
+'''
+
+headers={
+
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "content-length": "2289",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "cookie": "",
+    "origin": "https://healthreport.zju.edu.cn",
+    "referer": "https://healthreport.zju.edu.cn/ncov/wap/default/index?from=history",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+    "x-requested-with": "XMLHttpRequest"
+}
+
+referer = 'https://healthreport.zju.edu.cn/ncov/wap/default/index?from=history'
 loclist=["120.08654","30.30897"]#school
 
 cookie="_ga=GA1.3.1720262211.1602687713; eai-sess=6serm2rujdf07bg3n8iftc0ba4; UUkey=916d7bd103a4f2fdbfa761e07ef09ca8; Hm_lvt_48b682d4885d22a90111e46b972e3268=1603201784,1603252182,1603407288; Hm_lpvt_48b682d4885d22a90111e46b972e3268=1603407288"
@@ -29,7 +69,8 @@ data={
     'sqhzjkkys': '1','gwszgzcs': '0','szgj': '0','szgjcs': '0','fxyy': '0','jcjg': '0'
 }
 #print(loctext[loctext.index("formatted_address")+2])
-locget = requests.post(submiturl,data=data,headers={'user':user,'cookie':cookie})
+headers["cookie"]=cookie
+locget = requests.post(submiturl,data=data,headers=headers)
 print(re.findall(r"\"m\":\"[^\"]*\"",locget.text)[0])
 
 cookie="eai-sess=s4ng1njate3ptkfh6bkmsrc115; UUkey=b8f8156f7a65b37aa795cf4f7d61e9e0; _csrf=S8mwplVi9KWoF2WQ0TlCeFkhpR4lX1aJpoKgE2bhNX0%3D; _pv0=XRJlKUFMl2way3Zh6ZasvwUpdKno8L2FgghT2TAzUhzArWjlkn2N4xGmspk1JczgQzmxKOKFsVWJEmzcSHl%2FOX8BHYKiN6juLfyBPudRLkdQ4FlhUVivCfITkqg6YeD0iy6KMxI4RYKvloELvXgAVOabthpHYDwI2YtK0IinOJ%2FpIM%2BDBL3d51ziVVbfpM8vSbVXPbS9IfVOlOcXlQaNRfATVJWJTJx0hpMPny1wD9GJYG7tx1bhFSwjndKzfIE0Xib8vD9zt2JB52t8VfOOdyNGJOHFDIrhXXu%2Fi5Tyr2Lve7I6J2c%2Fynaw8T%2BBK3WBFePoFBqaJgYv1QX%2FhhccJL5ae4O2aqHz76C9DL71xUi0aQM8aYtC2E4spd%2BDr6F0KvHFet%2FOhkEKLRbjNU1ggUmO3mKO5M1LVNA7eDuaIvU%3D; _pf0=0xYOwppE7LKdiKaI8y69n1HYMYAAfdlvDe3GQKv5%2FsI%3D; _pc0=cjkYca4h2r1qV4GDJr2gv5di5ZCuHwCrqraB38kofv88QqqFw0cqLUPUdBj1tPPa; iPlanetDirectoryPro=0UC4LcbQU97EayPsiO%2B4Y9o50qkNPifkka2yyn83Cfl6BeN6KTOj%2BKWusRAqhgm9NBjF%2By0ISGuTEm22BYuHBijGLYj01A0CUlguRWffRXaAWggHn5iwMcYTLbtIi03XSDX98fjuv8YVPVh65PBOIVP3WdhKpQNtpD5k3BHaUdR%2FItJf5TLxDmAvbTUWsCA%2F%2BJUCpa%2Bp6Q4Dglj6%2FQuyQdcjcqXrO2N1ifNGB4NHp4MXfiWDD5Q9iXirc1s2qtmWARzxSWvCchA%2F9uHhiy4%2FkX1dUpjqoQckQYo2Zf8myRIBrBs%2FoZsY%2FAV7oI571rKSdc6iUxt8c1A3Bpc26ZMYsgdhWDiS9LmEGtlCCJiv%2Byo%3D; Hm_lvt_48b682d4885d22a90111e46b972e3268=1613312088; Hm_lpvt_48b682d4885d22a90111e46b972e3268=1613312125"
@@ -55,7 +96,8 @@ data={
     'jcqzrq': '0','jrsfqzys': '0','jrsfqzfy': '0','sfyqjzgc': '0','sfsqhzjkk': '1',
     'sqhzjkkys': '1','gwszgzcs': '0','szgj': '0','szgjcs': '0','fxyy': '0','jcjg': '0'
 }
-locget = requests.post(submiturl,data=data,headers={'user':user,'cookie':cookie})
+headers["cookie"]=cookie
+locget = requests.post(submiturl,data=data,headers=headers)
 print(re.findall(r"\"m\":\"[^\"]*\"",locget.text)[0])
 
 cookie="eai-sess=0pmj4ut8o9csel38nnp4ekatp0; UUkey=3905c42009c6f1fef356c8187bb79970; _csrf=S8mwplVi9KWoF2WQ0TlCeMAcAmuNhy4M4h39r0Cy9cQ%3D; _pv0=j81G1%2FThZangWheG7YjDhoivk43vPNCEgfG1dQ8cYW8oqRVhVWy4IAR8WWRgncV%2F8NCRLxo%2FK0slaNifXhcnndZ6wlWdteFqsrm1yvS93jhFUWlaacA5JHHl%2FFPGvmveXmz48%2FX6NXtJL7o5YK1tIAWrFqsdSE3Rc9xrKVEZaNOyFfI56L20tcoZIrzEVX0mAEIswPELtgZ5Cib3jUeVUMHGZeg5uZJMK7vlOQtpvjVpvcVvKjJ1WQvGVB%2Fw4P1JAWRybgJ2C7z%2FXj4ZJ8lhkHN1k3hNHJKs%2FIxgHyDjdEohiLAIBXgSrgYcYSXwlT%2BYQyyrClEPMYnmRApdE0W5pHkiAndBbplXw9hMIF%2F%2B%2FnlWuH9ICsewDDhRdLEB12%2FD3iX2cIrqJILdDimD5DsvmxkhysUCV2eQlMztVwHvp0s%3D; _pf0=%2F%2BSxyjfFA2wvZyO7sE%2B8ayUzLuJm7CEbqTn7Oh1tVD8%3D; _pc0=vsW8bzbjq1nTplByN6cJtast4ObHkhtJNevJe6pwnDNWulgwi5MwvboT2ohgtr24; iPlanetDirectoryPro=mURnkR9FyETqYG0iT4jVMKq63m%2FdYYJs6SeoZq%2BZIpeGeVUqUzvhOsyUo%2Bl28qMU42MtlGXzmml6yiI9zjKdx5hO0vR693%2F6Q62%2FWZM9rbuUU3EPd7oADL0AoJ3c85UVZm2BUjnSN2jH9hyvnNobimLUx2xcFkAd%2BUwSSp%2Fgc8zDmlSaOLWHLgMEAEHxUP12Y9hZqWMrWvi6mieCVziOA8G3CqRNIUIXxDmsvoswvpAfpjIMnju1aSqYGrKF5BEzDIBCcDEN7s77vQt%2BuTAQ29CsLqTltZbU%2FxC1QJMzPHYoFVILjq58is1JEDyNHAufzCsGdGjDyrTPWfKAhSaL6BUN%2Bl91xXYKmJbYwUfylxA%3D; Hm_lvt_48b682d4885d22a90111e46b972e3268=1615284291; Hm_lpvt_48b682d4885d22a90111e46b972e3268=1615284303"
@@ -81,5 +123,6 @@ data={
     'jcqzrq': '0','jrsfqzys': '0','jrsfqzfy': '0','sfyqjzgc': '0','sfsqhzjkk': '1',
     'sqhzjkkys': '1','gwszgzcs': '0','szgj': '0','szgjcs': '0','fxyy': '0','jcjg': '0'
 }
-locget = requests.post(submiturl,data=data,headers={'user':user,'cookie':cookie})
+headers["cookie"]=cookie
+locget = requests.post(submiturl,data=data,headers=headers)
 print(re.findall(r"\"m\":\"[^\"]*\"",locget.text)[0])
